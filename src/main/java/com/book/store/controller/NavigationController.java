7f4a3bc5f4178a264da7c28f9f9e6a19d2bd5a84
senan0144 <30856990+senan0144@users.noreply.github.com>
2019-01-29 00:07:15 +0400

    index page edited, imports edited, navigation controller added

--- src/main/java/com/book/store/controller/NavigationController.java
+++ src/main/java/com/book/store/controller/NavigationController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class NavigationController {
 
     @RequestMapping("/")
-    public String index(){
+    public String getIndexPage() {
         return "view/index";
     }
 
